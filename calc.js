20c3a895905b4f636a24f29e08f34cc09d491aa4
Jubril Issa <issa.jubril@andela.com>
2018-08-20 17:53:29 +0100

    Hackathon code for group3

---
+++ calc.js
@@ -0,0 +1,16 @@
+
+function ud(n){
+    let disp = document.getElementById("disp");
+    disp.innerHTML += n ;
+
+}
+function ans(){
+    c=eval(disp.innerHTML);
+    disp.innerHTML=c;
+}
+function clc(){
+    disp.innerHTML='';
+}
+
+
+  
